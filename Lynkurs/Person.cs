8b09207232bc6e20692da3cd1c62a167eb7098ab
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2025-08-22 15:57:39 +0200

    Initial commit

---
+++ Lynkurs/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lynkurs
+{
+    public class Person
+    {
+        // auto-implementerte egenskaper med verdi sjekk
+        private string personName; // global variabel og er synlig kun til alle metodene i Person klasse
+        public string PersonName
+        {
+            get { return personName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    Console.WriteLine("Du Du må gi et navn");
+
+                else
+                {
+                    personName = value;
+                }
+            }
+        }
+
+        private int personAge;
+        public int PersonAge
+        {
+            get { return personAge; }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Du må gi positivt tall til alderen");
+
+                else
+                {
+                    personAge = value;
+                }
+            }
+        }
+
+        // bruk av auto-implementerte egenskaper
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // bruk av instans variabler og set/get metoder
+        private string name;
+        private int age;
+
+        public void SetName(string _name)
+        {
+            name = _name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetAge(int _age)
+        {
+            age = _age;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+
+        // method overloading: to metoder har samme signatur men forskjellig antall parametere
+        public void Spiser()
+        {
+            Console.WriteLine(this.name + " spiser mat");
+        }
+
+        public void Spiser(string mat)
+        {
+            // num er in lokal variabel og er synlig kun i denne metoden
+            int num = 2;
+            Console.WriteLine($"{this.name} spiser {mat}");
+        }
+    }
+}
---
+++ Lynkurs/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lynkurs;
+using System;
+
+Console.WriteLine("Hello, World!");
+
+// variabler og datatyper
+int nummer = 30;
+string text1 = nummer.ToString();
+// int til string
+string text2 = "lynkurs";
+Console.WriteLine(text2[1]);
+// string til int
+int input = int.Parse(Console.ReadLine());
+
+
+// klasser og objekter
+
+// oppretter et objekt av klasse Person
+Person person = new Person();
+
+// ved bruk av instans variabler og set/get metoder
+person.SetName("Håkon");
+string personName = person.GetName();
+Console.WriteLine(personName);
+
+person.SetAge(20);
+// enten den
+int personAge = person.GetAge();
+Console.WriteLine(personAge);
+//eller den
+Console.WriteLine(person.GetAge());
+
+Person person1 = new Person();
+
+// auto-implementerte egenskaper
+person1.Name = "Elin";
+person1.Age = 19;
+Console.WriteLine($"Person {person1.Name} er {person1.Age} år gammel");
+
+// auto-implementerte egenskaper med verdi sjekk
+Person person2 = new Person();
+person2.PersonName = "Kristian";
+person2.PersonAge = -20;
+Console.WriteLine($"Person {person2.PersonName} er {person2.PersonAge} år gammel");
+Person person3 = new Person();
+
+person3.PersonName = "";
+person3.PersonAge = 22;
+Console.WriteLine($"Person {person3.PersonName} er {person3.PersonAge} år gammel");
+
+
+// metoder (metode overloading)
+person.Spiser();
+person.Spiser("Kylling");
+
+
+// if-betingelse og operatører
+int tall1 = 5;
+int tall2 = 10;
+
+// for å sammenligne tall1 med tall2 
+if (tall1 == tall2)
+    Console.WriteLine("Tallene er like.");
+else
+    Console.WriteLine("Tallene er ikke like.");
+
+// for å finne ut hvilket tall er det største
+if (tall1 > tall2) // denne skal alltid ha betingelse
+    Console.WriteLine($"Tall1 {tall1} er den største.");
+else if (tall2 > tall1)   // denne skal alltid ha betingelse
+    Console.WriteLine($"Tall2 {tall2} er den største.");
+else    // denne skal ikke ha betingelse
+    Console.WriteLine("Tallene er like");
+
+// for å finne ut om tallet er oddetall eller partall
+Console.WriteLine("Vennligst skriv et tall: ");
+int x = int.Parse(Console.ReadLine());
+if (x % 2 == 0)
+    Console.WriteLine($"Tallet {x} er partall.");
+else if (x % 2 == 1)
+    Console.WriteLine($"Tallet {x} er oddetall.");
+else
+    Console.WriteLine($"Tallet {x} er verken oddetall eller partall.");
